ca7ff9b3f4db8a6e60d2203252123e49c4605318
jafgh <jaafar221gh@gmail.com>
2025-06-05 13:34:42 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,22 @@
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // لتمكين WebView على Android (API >= 19)
-  if (Platform.isAndroid) WebView.platform = AndroidWebView();
-
-  runApp(const DalelkApp());
-}
+void main() => runApp(DalelkApp());
 
 class DalelkApp extends StatelessWidget {
-  const DalelkApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Dalelk1',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: const AppBarTheme(
-          color: Colors.indigo,
-          elevation: 2,
-          centerTitle: true,
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+        primarySwatch: Colors.blue,
       ),
-      home: const WebViewScreen(),
-    );
-  }
-}
-
-class WebViewScreen extends StatefulWidget {
-  const WebViewScreen({Key? key}) : super(key: key);
-
-  @override
-  State<WebViewScreen> createState() => _WebViewScreenState();
-}
-
-class _WebViewScreenState extends State<WebViewScreen> {
-  late final WebViewController _controller;
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    // تهيئة WebViewController
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onPageStarted: (String url) {
-            setState(() => _isLoading = true);
-          },
-          onPageFinished: (String url) {
-            setState(() => _isLoading = false);
-          },
-          onNavigationRequest: (NavigationRequest request) {
-            // إذا أردنا حظر روابط معينة يمكن هنا
-            return NavigationDecision.navigate;
-          },
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dalelk1'),
         ),
-      )
-      ..loadRequest(Uri.parse('https://dalelk1.netlify.app'));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Dalelk1'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.refresh),
-            tooltip: 'إعادة تحميل',
-            onPressed: () {
-              _controller.reload();
-            },
-          ),
-        ],
-      ),
-      body: Stack(
-        children: [
-          // WebView نفسه
-          WebViewWidget(controller: _controller),
-
-          // مؤشر تحميل (حلقات دائرية) عند تحميل الصفحة
-          if (_isLoading)
-            const Center(
-              child: CircularProgressIndicator(),
-            ),
-        ],
+        body: Center(
+          child: Text('مرحبًا بك في Dalelk1!'),
+        ),
       ),
     );
   }
